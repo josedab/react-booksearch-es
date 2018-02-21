bfbbb225a87f6692e61178d346eae66a7e4fab35
Jose David Baena <josedab@gmail.com>
2018-02-21 13:56:25 -0800

    Reactive search components added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  ReactiveBase,
+  DataSearch,
+  SingleRange,
+  ResultCard
+} from '@appbaseio/reactivesearch';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ReactiveBase
+        app="good-books-ds"
+        credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
+      >
+        <DataSearch
+          componentId="mainSearch"
+          dataField={["original_title", "original_title.search", "authors", "authors.search"]}
+          queryFormat="and"
+          iconPosition="left"
+        />
+        <SingleRange
+          componentId="ratingsFilter"
+          dataField="average_rating_rounded"
+          title="Book Ratings"
+          data={[
+            { start: 4, end: 5, label: "★★★★ & up" },
+            { start: 3, end: 5, label: "★★★ & up" },
+            { start: 2, end: 5, label: "★★ & up" },
+            { start: 1, end: 5, label: "★ & up" },
+          ]}
+          react={{
+            and: "mainSearch"
+          }}
+        />
+        <ResultCard
+          componentId="results"
+          dataField="original_title"
+          react={{
+            "and": ["mainSearch", "ratingsFilter"]
+          }}
+          onData={(res)=>({
+            "image": res.image,
+            "title": res.original_title,
+            "description":  res.average_rating + " ★ "
+          })}
+        />
+      </ReactiveBase>
     );
   }
 }
-
 export default App;
