7d397e7465784cde6caffa396839bf9a3206e152
Jose David Baena <josedab@gmail.com>
2018-02-21 18:45:14 -0800

    Added css for react search components - Updated React components to use css classNames

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   SingleRange,
   ResultCard
 } from '@appbaseio/reactivesearch';
+import './App.css';
+
 class App extends Component {
   render() {
     return (
@@ -12,40 +14,64 @@ class App extends Component {
         app="good-books-ds"
         credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
       >
-        <DataSearch
-          componentId="mainSearch"
-          dataField={["original_title", "original_title.search", "authors", "authors.search"]}
-          queryFormat="and"
-          iconPosition="left"
-        />
-        <SingleRange
-          componentId="ratingsFilter"
-          dataField="average_rating_rounded"
-          title="Book Ratings"
-          data={[
-            { start: 4, end: 5, label: "★★★★ & up" },
-            { start: 3, end: 5, label: "★★★ & up" },
-            { start: 2, end: 5, label: "★★ & up" },
-            { start: 1, end: 5, label: "★ & up" },
-          ]}
-          react={{
-            and: "mainSearch"
-          }}
-        />
-        <ResultCard
-          componentId="results"
-          dataField="original_title"
-          react={{
-            "and": ["mainSearch", "ratingsFilter"]
-          }}
-          onData={(res)=>({
-            "image": res.image,
-            "title": res.original_title,
-            "description":  res.average_rating + " ★ "
-          })}
-        />
+        <div className="navbar">
+          <div className="logo">
+            The Booksearch App
+          </div>
+          <DataSearch
+            componentId="mainSearch"
+            dataField={["original_title", "original_title.search", "authors", "authors.search"]}
+            queryFormat="and"
+            placeholder="Search for a book title or an author"
+            autosuggest={false}
+            className="datasearch"
+            innerClass={{
+              "input": "searchbox",
+              "list": "suggestionlist"
+            }}
+          />
+        </div>
+        <div className={"display"}>
+          <div className={"leftSidebar"}>
+            <SingleRange
+              componentId="ratingsFilter"
+              dataField="average_rating_rounded"
+              title="Book Ratings"
+              data={[
+                { start: 4, end: 5, label: "★★★★ & up" },
+                { start: 3, end: 5, label: "★★★ & up" },
+                { start: 2, end: 5, label: "★★ & up" },
+                { start: 1, end: 5, label: "★ & up" },
+              ]}
+            />
+          </div>
+          <div className={"mainBar"}>
+            <ResultCard
+              componentId="results"
+              dataField="original_title"
+              react={{
+                "and": ["mainSearch", "ratingsFilter"]
+              }}
+              pagination={true}
+              size={8}
+              onData={(res)=>(
+                {
+                  "image": res.image,
+                  "title": res.original_title,
+                  "description":  res.average_rating + " ★ "
+                }
+              )}
+              className="result-data"
+              innerClass={{
+                "image": "result-image",
+                "resultStats": "result-stats"
+              }}
+            />
+          </div>
+        </div>
       </ReactiveBase>
     );
   }
 }
+
 export default App;
